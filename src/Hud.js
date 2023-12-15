a077f3ebf28b42488de81bbc910fbeb5c45cee96
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2023-12-16 00:31:44 +0100

    Setup basic UI for now

--- src/Hud.js
+++ src/Hud.js
@@ -1,14 +1,13 @@
+import { SearchInput, TextInput } from 'evergreen-ui'
+
 export default function Hud() {
     return (
-        <div className="map-ui w-full h-full p-2 grid grid-cols-12 gap-4 grid-rows-[auto,1fr]">
-            <div className="col-span-3">
-                <h1 className="bg-green-500">whatever</h1>
+        <div id="map-ui" className="map-ui w-full h-full p-2">
+            <div className="flex gap-3">
+                <SearchInput placeholder="Location" />
+                <TextInput name="text-input-name" placeholder="Text input placeholder..." />
             </div>
-            <div className="col-span-2">
-                <h1 className="bg-blue-500">Price</h1>
-            </div>
-            <div className="col-span-7" />
-            <div className="row-span-2 col-span-3 h-full bg-green-500">Search results</div>
+            <div className="w-128 max-w-xl h-full rounded-md bg-green-500">Search results</div>
         </div>
     )
 }
